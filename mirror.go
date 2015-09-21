cb36567b750f972f0cce374371d1eb785d9f55c8
prashant <agarwalaprashant1991@gmail.com>
2015-09-21 21:37:04 +0530

    channel added

--- mirror.go
+++ mirror.go
@@ -8,64 +8,95 @@ import (
     "net/url"
     "io"
     "golang.org/x/net/html"
+    "strings"
+    "path/filepath"
 )
 
-func retrieve(uri string){
+func createPaths (full_path string) io.Writer{
 
-    parsed_url, err := url.Parse(uri)
+  dir, file := filepath.Split(full_path)
+  fmt.Println(full_path)
+  if ((len(dir) == 0) || (len(file) == 0)){
+    file = "index.html"
+  }
+  if(len(dir)>0){
+    err := os.MkdirAll(dir, 0777)
     if(err != nil){
-      fmt.Println("Url Parsing Error: ",err)
+        fmt.Println("Directory Create Error: ",dir, err)
+        os.Exit(1)
+    }
+  }
+  fileWriter, err := os.Create(dir+file)
+
+  if(err != nil){
+      fmt.Println("File Open Error: ",err)
       os.Exit(1)
-    }
-    resp, err := http.Get(uri)
+  }
+  fmt.Println("file created successfully",full_path)
+  return fileWriter
+}
 
-    if(err != nil){
-        fmt.Println("Http Transport Error: ",err)
-        os.Exit(1)
-    }
-    dir_path := parsed_url.Host+parsed_url.Path
-    dir := os.MkdirAll(dir_path, 0777)
+func generatelinks (resp_reader io.Reader, uri string, ch chan string){
+  z := html.NewTokenizer(resp_reader)
+  fmt.Println("finding links in", uri)
+  countLinks := 0
+  for{
+      tt := z.Next();
+      switch{
+          case tt==html.ErrorToken:
+              fmt.Println("Total number of links: ", countLinks)
+              return
+          case tt==html.StartTagToken:
+              t := z.Token()
+              fmt.Println(t.Data)
+              if t.Data == "a"{
+                  for _,a := range t.Attr{
+                      if a.Key == "href"{
+                        if(strings.Index(a.Val, uri) == 0){
+                          //fmt.Println("Link: ", a.Val)
+                          countLinks++
+                          ch <- a.Val
+                          }
+                          break;
+                      }
 
-    if(dir != nil){
-        fmt.Println("Directory Create Error: ",dir)
-        os.Exit(1)
-    }
+                  }
 
-    fileWriter, err := os.Create(dir_path+"file.txt")
+              }
+      }
+  }
+  fmt.Println("links finding complete", uri)
+}
 
-    if(err != nil){
-        fmt.Println("File Open Error: ",err)
-        os.Exit(1)
-    }
+func retrieve(ch chan string) {
 
-    resp_reader := io.TeeReader(resp.Body, fileWriter)
+    select {
+      case uri := <-ch:
+          parsed_url, err := url.Parse(uri)
+          if(err != nil){
+            fmt.Println("Url Parsing Error: ",err)
+            os.Exit(1)
+          }
+          fmt.Println("Http request", uri)
+          resp, err := http.Get(uri)
+          fmt.Println("Http response", uri)
 
-    z := html.NewTokenizer(resp_reader)
-    countLinks := 0
-    for{
-        tt := z.Next();
-        switch{
-            case tt==html.ErrorToken:
-                fmt.Println("Total number of links: ", countLinks)
-                return
-            case tt==html.StartTagToken:
-                t := z.Token()
-
-                if t.Data == "a"{
-                    countLinks++
-                    for _,a := range t.Attr{
-                        if a.Key == "href"{
-                            fmt.Println("Link: ", a.Val)
-                            break;
-                        }
-
-                    }
-
-                }
-        }
-    }
+          if(err != nil){
+              fmt.Println("Http Transport Error: ",err)
+              os.Exit(1)
+          }
+          full_path := parsed_url.Host+parsed_url.Path
+          fileWriter:= createPaths(full_path)
+          resp_reader := io.TeeReader(resp.Body, fileWriter)
+          fmt.Println("file saved successfully")
+          generatelinks(resp_reader,uri,ch)
+          retrieve(ch)
+      default:
+          fmt.Println("no message received")
+      }
 
 }
+
 
 func main(){
     flag.Parse()
@@ -75,5 +106,7 @@ func main(){
         fmt.Println("Specify a start url")
         os.Exit(1)
      }
-    retrieve(args[0])
+    ch := make(chan string,10000)
+    ch <- args[0]
+    retrieve(ch)
 }
